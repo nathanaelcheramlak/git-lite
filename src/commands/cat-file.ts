2b220de9cd28c8def7c3e1f2e4ef3651e06faad2
Nathanael <nathanaelcheramlak7@gmail.com>
2025-07-03 22:51:31 +0300

    feat: new flags and refactor the code

--- src/commands/cat-file.ts
+++ src/commands/cat-file.ts
@@ -1,30 +1,62 @@
 import * as fs from "fs";
 import { inflateSync } from "zlib";
+import * as path from "path";
 import getRootDir from "../utils/get-root-dir";
+import manualCatFile from "../manuals/cat-file.manual";
+import type ParsedArgsType from "../types/arg-parser.type";
 
-export default function handleCatFile(flag: string, objectHash: string): void {
-  const objectPath =
-    getRootDir(process.cwd()) +
-    "/.git-lite/objects/" +
-    objectHash.slice(0, 2) +
-    "/" +
-    objectHash.slice(2);
-  console.log(objectPath);
+export default function handleCatFile(args: ParsedArgsType): void {
+  if (args.hash.length === 0 || args.hash[0] === undefined) {
+    manualCatFile();
+    return;
+  }
 
-  switch (flag) {
-    case "-p":
-      if (!fs.existsSync(objectPath)) {
-        console.log("Hash not found!");
-        return;
-      }
+  const hash = args.hash[0];
+  const objectPath = path.join(
+    getRootDir(),
+    ".git-lite",
+    "objects",
+    hash.slice(0, 2),
+    hash.slice(2)
+  );
 
-      const buffer = fs.readFileSync(objectPath);
-      const data = inflateSync(buffer).toString("utf-8");
-      const fileContent = data.split(/\u0000/g)[1];
+  let data;
+  let fileHeader;
 
+  try {
+    if (!fs.existsSync(objectPath)) {
+      console.error("git-lite: hash doesn't exist");
+      process.exit(1);
+    }
+
+    const buffer = fs.readFileSync(objectPath);
+    data = inflateSync(buffer).toString("utf-8");
+    fileHeader = data.split("\u0000")[0] ?? "";
+  } catch (error) {
+    console.error(
+      "git-lite: failed to read object —",
+      (error as Error).message
+    );
+    process.exit(1);
+  }
+
+  switch (true) {
+    case args.flags.p:
+      const fileContent = data.split("\u0000")[1];
       process.stdout.write(fileContent ?? "");
       break;
+
+    case args.flags.t:
+      const fileType = fileHeader.split(" ")[0];
+      console.log(fileType);
+      break;
+
+    case args.flags.s:
+      const fileSize = fileHeader.split(" ")[1];
+      console.log(fileSize);
+      break;
+
     default:
-      console.log(`cat-file does not have a ${flag} flag`);
+      manualCatFile();
   }
 }

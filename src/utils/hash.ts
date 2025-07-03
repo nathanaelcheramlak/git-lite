6c13232649d903ef810dcef96377ff4ef2d8768f
Nathanael <nathanaelcheramlak7@gmail.com>
2025-07-04 01:16:47 +0300

    feat: hash checker function

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -5,4 +5,10 @@ function getHash(content: string, hashFunction: string): string {
   return hash.update(content).digest("hex");
 }
 
-export { getHash };
+function isHash(value: string | undefined | null): boolean {
+  if (typeof value !== "string") return false;
+  const SHA1_REGEX = /^[a-f0-9]{40}$/i;
+  return SHA1_REGEX.test(value.trim());
+}
+
+export { getHash, isHash };

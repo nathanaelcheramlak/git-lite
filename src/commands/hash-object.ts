99d23b83ef562a17ea76af11be8275bc2b7c9c91
Nathanael <nathanaelcheramlak7@gmail.com>
2025-07-04 23:10:04 +0300

    removed support for types

--- src/commands/hash-object.ts
+++ src/commands/hash-object.ts
@@ -5,38 +5,14 @@ import { deflateSync } from "zlib";
 import type ParsedArgsType from "../types/arg-parser.type";
 import manualHashObject from "../manuals/hash-object.manual";
 
-enum fileTypeEnum {
-  blob = "blob",
-  tree = "tree",
-  commit = "commit",
-}
-
 export default function handleHashObject(args: ParsedArgsType): string | void {
   if (args.positionals.length === 0) {
     manualHashObject();
     return;
   }
+  let filePath = args.positionals[0];
 
-  const typeStr = args.positionals[0];
-  const isValidType =
-    typeStr && Object.values(fileTypeEnum).includes(typeStr as fileTypeEnum);
-  if (args.flags.t && (!typeStr || typeStr === "")) {
-    console.error("git-lite: -t flag was used but type wasn't provided");
-    return;
-  }
-
-  let filePath = undefined;
-  let fileType = undefined;
-
-  if (isValidType) {
-    fileType = args.positionals[0];
-    filePath = args.positionals[1];
-  }
-
-  filePath = filePath ?? args.positionals[0];
-  fileType = fileType ?? "blob";
-
-  if (filePath === undefined || fileType === undefined) {
+  if (!filePath) {
     console.error("git-lite: file not provided");
     return;
   }
@@ -47,7 +23,7 @@ export default function handleHashObject(args: ParsedArgsType): string | void {
   try {
     fileContent = fs.readFileSync(filePath, "utf-8");
     const fileSize = Buffer.byteLength(fileContent, "utf-8");
-    const blobHeader = `${fileType} ${fileSize}\u0000`;
+    const blobHeader = `blob ${fileSize}\u0000`;
     const storeContent = blobHeader + fileContent;
 
     fileHash = getHash(storeContent, "SHA-1");
